4bf17cd5a222e10e53a7bb9703ab0d02a8502b5b
Md. Abu Sayeed Mondal <sayeedmondal1412@gmail.com>
2022-02-23 11:22:05 +0600

    remove smartcontract page

--- pages/create-smart-contract.js
+++
@@ -1,9 +0,0 @@
-const CreateSmartContract = () => {
-    return (
-        <div>
-            <p className="enableMetaMask" onClick={createSmar}>
-                Create a smart contract
-            </p>
-        </div>
-    );
-};
